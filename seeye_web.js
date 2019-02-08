b7d28e1cf52fb0c4134434b94686854ba247cc1d
Jiongsheng Cai <johson@Jiongshengs-MacBook-Pro.local>
2019-02-07 20:35:41 -0500

    two-column category, one-row item

--- seeye_web.js
+++ seeye_web.js
@@ -66,9 +66,9 @@ function seeyeClick(seeye_button) {
 	var seeye_popup = document.getElementsByClassName("ytp-popup ytp-seeye-menu")[0]
 
 	if (seeye_popup.style.display == "none") {
-		console.log("switch to inline")
+		// console.log("switch to inline")
 		seeye_popup.style.display = "inline"
-		console.log(seeye_popup.style.display)
+		// console.log(seeye_popup.style.display)
 	}
 	else {
 		console.log("switch to none")
@@ -78,33 +78,36 @@ function seeyeClick(seeye_button) {
 }
 
 
-function createSeeyeItem(img_src, name, link) {
+function createSeeyeItem(img_src, name, link, price) {
 	"Create a single product item."
 
 	var movie_player = document.getElementById('movie_player')
 
 	var seeye_item = document.createElement("DIV")
 	seeye_item.className="seeye-item"
-	seeye_item.style.width="inherit"
-	seeye_item.style.height="inherit"
+	seeye_item.style.width="200px"
+	seeye_item.style.height="80px"
+	seeye_item.style.margin="15px"
 	seeye_item.style.textAlign="center"
 
 	var a = document.createElement("A")
 	a.href = link
-	a.style.width="inherit"
 
 	var img = document.createElement("IMG")
 	img.src = img_src
-	img.style.width= "80%"
-	img.style.marginTop= "20px"
+	img.style.width= "60px"
+	img.style.height= "70px"
+	img.style.float="left"
 
 	var p = document.createElement("P")
 	p.innerHTML = name
+	p.style.display="inline"
 
 	var rating = document.createElement("DIV")
 	rating.className="rating-star"
 	rating.style.position="relative"
-	rating.style.left=movie_player.offsetWidth / 6 - 31 + "px"
+	rating.style.left=movie_player.offsetWidth / 6 + "px"
+	rating.style.height="15px"
 
 	var star_outer = document.createElement("DIV")
 	star_outer.className="star-outer"
@@ -129,10 +132,15 @@ function createSeeyeItem(img_src, name, link) {
 	rating.appendChild(star_outer)
 	rating.appendChild(star_inner)
 	
+	var p2 = document.createElement("P")
+	p2.innerHTML = "Price: $" + price
+	p2.style.display="inline"
+
 	// Append
 	a.appendChild(img)
 	a.appendChild(p)
 	a.appendChild(rating)
+	a.appendChild(p2)
 	seeye_item.appendChild(a)
 
 	return seeye_item
@@ -143,19 +151,25 @@ function createTwoCategory(catg1, catg2, img_src1, img_src2) {
 
 	var seeye_two_catg = document.createElement("DIV")
 	seeye_two_catg.className="seeye-two-catg"
-	seeye_two_catg.style.width="inherit"
+	seeye_two_catg.style.width="200px"
 	seeye_two_catg.style.textAlign="center"
 
 	// two-column-style, float:left
 	var left = document.createElement("DIV")
 	left.style.width="50%"
 	left.style.float="left"
+	left.style.cursor="pointer"
+	left.onclick=function () {
+		navigate_to_category(catg1)
+	} 
 	var right = document.createElement("DIV")
-	right.style.width="inherit"
+	right.style.width="50%"
+	right.style.float="right"
 
 	var img1 = document.createElement("IMG")
 	img1.src = img_src1
-	img1.style.width= "90%"
+	img1.style.width= "90px"
+	img1.style.height= "100px"
 	img1.style.marginTop= "10px"
 	var p1 = document.createElement("P")
 	p1.innerHTML = catg1
@@ -163,7 +177,8 @@ function createTwoCategory(catg1, catg2, img_src1, img_src2) {
 
 	var img2 = document.createElement("IMG")
 	img2.src = img_src2
-	img2.style.width= "45%"
+	img2.style.width= "90px"
+	img2.style.height= "100px"
 	img2.style.marginTop= "10px"
 	var p2 = document.createElement("P")
 	p2.innerHTML = catg2
@@ -184,8 +199,10 @@ function createSeeyeCategory(seeye_popup) {
 
 	seeye_two_catg1 = createTwoCategory("Table", "Bed", "https://slimages.macysassets.com/is/image/MCY/products/9/optimized/3773449_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$", "https://slimages.macysassets.com/is/image/MCY/products/7/optimized/8893527_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$")
 	seeye_two_catg2 = createTwoCategory("Sofa", "Cabinet", "https://res-5.cloudinary.com/goedeker-s/image/upload/d_notavl.jpg/v1/media/catalog/product/s/i/simmons-6485-03-albany-slate.jpg", "https://scontent-amt2-1.cdninstagram.com/vp/52d57e1043152edcf4830fd6cf743410/5C925FB1/t51.2885-15/e35/44576124_270297947005818_8443078414178563520_n.jpg")
+	seeye_two_catg3 = createTwoCategory("Chair", "Other", "https://secure.img2-fg.wfcdn.com/im/93476064/resize-h800-w800%5Ecompr-r85/2805/28051632/Aletha+Solid+Wood+Dining+Chair.jpg", "https://secure.img2-fg.wfcdn.com/im/93476064/resize-h800-w800%5Ecompr-r85/2805/28051632/Aletha+Solid+Wood+Dining+Chair.jpg")
 	seeye_popup.appendChild(seeye_two_catg1)
 	seeye_popup.appendChild(seeye_two_catg2)
+	seeye_popup.appendChild(seeye_two_catg3)
 }
 
 function createSeeyePopup() {
@@ -197,37 +214,44 @@ function createSeeyePopup() {
 	seeye_popup.style.display="none"
 	seeye_popup.style.right="0"
 	seeye_popup.style.zIndex="10"
-	seeye_popup.style.overflow="scroll"
+	seeye_popup.style.overflowY="scroll"
 
-	// // Create popup panel
-	// var seeye_popup_panel = document.createElement("DIV")
-	// seeye_popup_panel.className="ytp-panel"
-
-	// // Append
-	// seeye_popup.appendChild(seeye_popup_panel)
-
-	// Create seeye items
-	var img_src = []
-	var name = []
-	var link = []
-	
-	img_src.push("https://slimages.macysassets.com/is/image/MCY/products/9/optimized/3773449_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$")
-	name.push("Sunset coffee table")
-	link.push("https://www.macys.com/shop/product/sunset-coffee-table-quick-ship?ID=2911648&CategoryID=35423#fn=sp%3D1%26spc%3D1538%26ruleId%3D129%7CBOOST%20SAVED%20SET%7CBOOST%20ATTRIBUTE%26searchPass%3DmatchNone%2")
-	
-	img_src.push("https://slimages.macysassets.com/is/image/MCY/products/7/optimized/8893527_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$")
-	name.push("Brodyn Queen Bed")
-	link.push("https://www.macys.com/shop/product/brodyn-queen-bed-quick-ship?ID=5157496&tdp=cm_app~zMCOM-NAVAPP~xcm_zone~zPDP_ZONE_B~xcm_choiceId~zcidM06MAU-0a1a6c85-584a-4fd9-9668-021302acb95a%40H8%40customers%2Ba")
-	
-	// TODO
-	// for (var i = 0; i < img_src.length; i++) {
-		// seeye_item = createSeeyeItem(img_src[i], name[i], link[i])
-		// seeye_popup.appendChild(seeye_item)
-	// }
 
 	createSeeyeCategory(seeye_popup)
 
 	return seeye_popup
+}
+
+function navigate_to_category(catg) {
+	"When click on a category, navigate to that category page."
+
+	// Clear the panel
+	var catg_div = document.getElementsByClassName("seeye-two-catg")
+	for (var i = catg_div.length - 1; i >= 0; i--) {
+		catg_div[i].parentElement.removeChild(catg_div[i])
+	}
+
+	// Create seeye items
+	var seeye_popup = document.getElementsByClassName("ytp-popup ytp-seeye-menu")[0]
+	var img_src = []
+	var name = []
+	var link = []
+	var price = []
+	
+	img_src.push("https://secure.img1-fg.wfcdn.com/im/38774897/resize-h800%5Ecompr-r85/4381/43817957/Alsey+Rustic+Teak+and+4+Drawer+Accent+Chest.jpg")
+	name.push("Alsey Rustic Teak and 4 Drawer Accent Chest")
+	link.push("https://www.wayfair.com/furniture/pdp/foundry-select-alsey-rustic-teak-and-4-drawer-accent-chest-fnds1469.html")
+	price.push("346.99")
+
+	img_src.push("https://secure.img1-fg.wfcdn.com/im/13525664/resize-h800%5Ecompr-r85/3932/39325902/Idella+Side+Table.jpg")
+	name.push("Idella Side Table")
+	link.push("https://www.wayfair.com/outdoor/pdp/union-rustic-idella-side-table-unrs1441.html")
+	price.push("196.99")
+	
+	for (var i = 0; i < img_src.length; i++) {
+		seeye_item = createSeeyeItem(img_src[i], name[i], link[i], price[i])
+		seeye_popup.appendChild(seeye_item)
+	}
 }
 
 
